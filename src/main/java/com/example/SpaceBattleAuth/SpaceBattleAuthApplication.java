06c05668a50ada838e25b834b728745627ce8f87
YuriPetukhov <petuchowjuri@yahoo.de>
2025-04-05 22:22:56 +0300

    initial commit

---
+++ src/main/java/com/example/SpaceBattleAuth/SpaceBattleAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpaceBattleAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceBattleAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceBattleAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpaceBattleAuth/SpaceBattleAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpaceBattleAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceBattleAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
